acb3e04d30889c904786b8ee0fe0617d72a10374
Shreya Surendra Choure <schoure@pdx.edu>
2023-04-26 12:38:58 -0700

    HW2 Completed

--- hw2/04-background.js
+++ hw2/04-background.js
@@ -1,1 +1,39 @@
-// Add your code here
+const toggleButton = document.getElementById("toggleButton");
+const input = document.getElementById("number");
+const container = document.getElementById("container");
+
+getRandomNumber = (max) => {
+	return Math.floor(Math.random() * max);
+};
+
+changeBackgroundColor = (maxRGB = 255) => {
+	const red = getRandomNumber(maxRGB);
+	const green = getRandomNumber(maxRGB);
+	const blue = getRandomNumber(maxRGB);
+	const alpha = 0.5; //to dim the color value
+	container.style.backgroundColor = `rgba(${red}, ${green}, ${blue}, ${alpha})`;
+};
+
+let intervalDuration = setInterval(() => {
+	changeBackgroundColor();
+}, 3000);
+
+
+
+//toggle Button to start and stop
+handleChange = (event) => {
+	if (toggleButton.value === "Start") {
+		let userDuration = input.value * 1000;
+		intervalDuration = setInterval(changeBackgroundColor, userDuration);
+		toggleButton.value = "Stop";
+		toggleButton.className = "btn btn-danger my-3";
+	} else {
+		clearInterval(intervalDuration);
+		input.value = "";
+		toggleButton.value = "Start";
+		toggleButton.className = "btn btn-primary my-3";
+	}
+	event.preventDefault();
+};
+
+toggleButton.addEventListener("click", handleChange);
--- hw2/05-form.js
+++ hw2/05-form.js
@@ -1,1 +1,63 @@
-// Add your code here
+const formSubmit = (event) => {
+	//empty form
+	if (!fullName.value && !email.value) {
+		console.warn("You must enter name and email to submit this form");
+	}
+
+	//valid form
+	else {
+		console.group("======== Form Submission ========");
+
+		//full-name
+		if (!fullName.value) {
+			console.log(`Name: No Submission`);
+		} else {
+			console.log(`Name: ${fullName.value}`);
+		}
+
+		//email
+		if (!email.value) {
+			console.log(`Email: No Submission`);
+		} else {
+			console.log(`Email: ${email.value}`);
+		}
+
+		//registration status
+		if (!registration.value) {
+			console.log(`Registration Status: No Submission`);
+		} else {
+			console.log(`Registration Status: ${registration.value}`);
+		}
+
+		//courses
+		const coursesTaken = [];
+		courses.forEach((course) => {
+			if (course.checked) {
+				coursesTaken.push(course.value);
+			}
+		});
+		if (coursesTaken.length == 0) {
+			console.log(`Courses: No Submission`);
+		} else {
+			console.log(`Courses: ${coursesTaken}`);
+		}
+
+		//comments
+		if (!comments.value) {
+			console.log(`Comments: No Submission`);
+		} else {
+			console.log(`Comments: ${comments.value}`);
+			console.groupEnd();
+		}
+	}
+	event.preventDefault();
+};
+
+const form = document.getElementById("form");
+const fullName = document.getElementById("name");
+const email = document.getElementById("email");
+const registration = document.getElementById("registration");
+const courses = document.getElementsByName("courses");
+const comments = document.getElementById("comments");
+
+form.addEventListener("submit", formSubmit);
